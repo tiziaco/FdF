230cf4c2f69653bd51080967a790048b12a4e9be
tiziaco <tiziano.iaco@gmail.com>
2024-01-02 14:07:39 +0100

    main

--- include/fdf.h
+++ include/fdf.h
@@ -6,10 +6,22 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:26:04 by tiacovel          #+#    #+#             */
-/*   Updated: 2023/12/22 11:42:24 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/01/02 13:25:22 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../src/gnl/get_next_line.h"
 #include <math.h>
 #include <fcntl.h>
+#include <mlx.h>
+#include <X11/keysym.h>
+
+#define MLX_ERROR	1
+#define WIN_WIDTH	400
+#define WIN_HEIGHT	400
+
+typedef struct	data_s
+{
+	void	*mlx_ptr;
+	void	*mlx_win;
+}	t_mlx_data;
--- main.c
+++ main.c
@@ -6,24 +6,19 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:33:16 by tiacovel          #+#    #+#             */
-/*   Updated: 2023/12/22 12:40:05 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/01/02 14:00:24 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/fdf.h"
 #include <stdio.h>
 
-int	main(int argc, char **argv)
+int	read_map(char *file)
 {
-	int fd;
-	char *line;
+	int		fd;
+	char	*line;
 
-	if (argc != 2)
-	{
-		printf("ERROR: please check input arguments!");
-		return (1);
-	}
-	fd = open(argv[1], O_RDWR);
+	fd = open(file, O_RDWR);
 	if (fd == -1)
 	{
 		printf("ERROR: please check the file name!");
@@ -38,5 +33,47 @@ int	main(int argc, char **argv)
 		free(line);
 	}
 	close(fd);
+	return(0);
+}
+
+int	handle_input(int keysym, t_mlx_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		printf("The %d key (ESC) has been pressed\n\n", keysym);
+		mlx_destroy_window(data->mlx_ptr, data->mlx_win);
+		mlx_destroy_display(data->mlx_ptr);
+		free(data->mlx_ptr);
+		exit(1);
+	}
+	printf("The %d key has been pressed\n\n", keysym);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_mlx_data	data;
+
+	if (argc != 2)
+	{
+		printf("ERROR: please check input arguments!");
+		return (EXIT_FAILURE);
+	}
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.mlx_win = mlx_new_window(data.mlx_ptr, 
+								WIN_WIDTH,
+								WIN_HEIGHT,
+								"My first window!");
+	if (data.mlx_win == NULL)
+	{
+		mlx_destroy_display(data.mlx_ptr);
+		free(data.mlx_ptr);
+		return (MLX_ERROR);
+	}
+	read_map(argv[1]);
+	mlx_key_hook(data.mlx_win, handle_input, &data);
+	mlx_loop(data.mlx_ptr);
 	return (0);
 }
