9bbb082b13f2c154b86e0b392c38357b19dddbac
tiziaco <tiziano.iaco@gmail.com>
2024-01-18 11:18:24 +0100

    refactor projection code

--- src/transform.c
+++ src/transform.c
@@ -6,13 +6,13 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:20:43 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/01/17 16:41:11 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/01/18 10:53:34 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-void print_matrix_uv(t_matrix *head)
+/* void print_matrix_uv(t_matrix *head)
 {
 	t_matrix	*rp;
 	t_matrix	*dp ;
@@ -29,83 +29,49 @@ void print_matrix_uv(t_matrix *head)
 		printf("\n");
 		dp = dp->down;
 	}
-}
-
-typedef struct {
-	float **m;
-} t_tr_matrix;
-
-t_tr_matrix allocate_tr_matrix()
-{
-	t_tr_matrix	matrix;
-	int			i;
-
-	matrix.m = (float **)malloc(3 * sizeof(float *));
-	if (matrix.m == NULL) {
-		exit(EXIT_FAILURE);
-	}
-	i = 0;
-	while (i < 3) {
-		matrix.m[i] = (float *)malloc(3 * sizeof(float));
-		if (matrix.m[i] == NULL) {
-			exit(EXIT_FAILURE);
-		}
-		i++;
-	}
-	return matrix;
-}
+} */
 
 static float	deg_to_rad(float degrees)
 {
 	return (degrees * (M_PI / 180.0));
 }
 
-void free_tr_matrix(t_tr_matrix matrix)
+static void rotate_nodes(t_node_tri *coor, float alpha, float beta, float gamma)
 {
-    int i = 0;
+	int previous_x;
+	int previous_y;
 
-    while (i < 3)
+	previous_y = coor->y;
+	if (alpha != 0)
 	{
-        free(matrix.m[i]);
-        i++;
-    }
-    free(matrix.m);
-}
-
-static t_tr_matrix	get_perspective_matrix(float pitch, float yaw, float roll)
-{
-	t_tr_matrix	matrix;
-
-	matrix = allocate_tr_matrix();
-	pitch = deg_to_rad(pitch);
-	yaw = deg_to_rad(yaw);
-	roll = deg_to_rad(roll);
-	matrix.m[0][0] = cos(yaw) * cos(roll) + sin(pitch) * sin(yaw) * sin(roll);
-	matrix.m[0][1] = -cos(pitch) * sin(roll);
-	matrix.m[0][2] = sin(yaw) * cos(roll) - sin(pitch) * cos(yaw) * sin(roll);
-	matrix.m[1][0] = cos(yaw) * sin(roll) - sin(pitch) * sin(yaw) * cos(roll);
-	matrix.m[1][1] = cos(pitch) * cos(roll);
-	matrix.m[1][2] = sin(yaw) * sin(roll) + sin(pitch) * cos(yaw) * cos(roll);
-	matrix.m[2][0] = cos(pitch) * sin(yaw);
-	matrix.m[2][1] = sin(pitch);
-	matrix.m[2][2] = cos(pitch) * cos(yaw);
-	return (matrix);
+		coor->y = previous_y * cos(alpha) + coor->z * sin(alpha);
+		coor->z = -previous_y * sin(alpha) + coor->z * cos(alpha);
+	}
+	previous_x = coor->x;
+	if (beta != 0)
+	{
+		coor->x = previous_x * cos(beta) + coor->z * sin(beta);
+		coor->z = -previous_x * sin(beta) + coor->z * cos(beta);
+	}
+	previous_x = coor->x;
+	previous_y = coor->y;
+	if (gamma != 0)
+	{
+		coor->x = previous_x * cos(gamma) - previous_y * sin(gamma);
+		coor->y = previous_x * sin(gamma) + previous_y * cos(gamma);
+	}
 }
 
 static void	get_projected_coord(t_matrix *matrix, t_view view, t_origin origin)
 {
-	t_tr_matrix	transformation_matrix;
 	t_node_tri	new_node;
 
-	transformation_matrix = get_perspective_matrix(view.rot_x, view.rot_y, view.rot_z);
 	new_node.x = matrix->x * view.zoom;
 	new_node.y = matrix->y * view.zoom;
 	new_node.z = matrix->z * view.zoom * 0.5;
-	matrix->pj_cor.x = new_node.x * transformation_matrix.m[0][0] + new_node.y * transformation_matrix.m[0][1] + new_node.z * transformation_matrix.m[0][2];
-	matrix->pj_cor.y = new_node.x * transformation_matrix.m[1][0] + new_node.y * transformation_matrix.m[1][1] + new_node.z * transformation_matrix.m[1][2];
-	matrix->pj_cor.x += origin.x0;
-	matrix->pj_cor.y += origin.y0;
-	free_tr_matrix(transformation_matrix);
+	rotate_nodes(&new_node, deg_to_rad(view.rot_x), deg_to_rad(view.rot_y), deg_to_rad(view.rot_z));
+	matrix->pj_cor.x = new_node.x + origin.x0;
+	matrix->pj_cor.y = new_node.y + origin.y0;
 }
 
 void	transform_nodes(t_matrix *matrix, t_view view, t_origin origin)
